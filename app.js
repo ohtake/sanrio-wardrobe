a73100037801b7f5cba2e5dfbfa2479863eb63f6
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2016-05-28 01:19:21 +0900

    Use justified-layout and react-lightbox-component

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Gallery from 'react-photo-gallery';
+import JustifiedLayout from 'justified-layout';
+import Lightbox from 'react-lightbox-component';
 import Promise from 'es6-promise'; // For older browsers http://caniuse.com/#feat=promises
 import fetch from 'whatwg-fetch';
 import yaml from 'js-yaml';
@@ -35,16 +36,17 @@ class App extends React.Component{
         let photos = data.map(obj => {
             let aspectRatio = 1.0 * obj.size.width_o / obj.size.height_o;
             return {
+                title: obj.title,
                 src: obj.image,
                 width: obj.size.width_o,
                 height: obj.size.height_o,
                 aspectRatio: aspectRatio,
-                lightboxImage: { src: obj.image, caption: (
+                description: (
                   <div>
                     <a href={obj.source} target="_blank">{obj.title}</a>
                     <ul>{obj.notes.map( n => { return <li>{n}</li> })}</ul>
                   </div>
-                )}
+                )
             };
         });
         this.setState({
@@ -56,8 +58,30 @@ class App extends React.Component{
         return(
             <div className="App">
                 {this.state.message ? <div>{this.state.message}</div> : null}
-                {this.state.photos ? <Gallery photos={this.state.photos} /> : null}
+                {this.state.photos ? this.renderGallery() : null}
             </div>
+        );
+    }
+    renderGallery(){
+        let geometory = JustifiedLayout(this.state.photos.map(p => { return p.aspectRatio; }), {targetRowHeight:80});
+        let imgs = this.state.photos.map(p => {
+            return {src:p.src, title: p.title, description: p.description};
+        });
+        let renderImageFunc = (idx, image, toggleLightbox, width, height) => {
+          return (
+            <img
+              key={idx}
+              src={ !!image.thumbnail ? image.thumbnail : image.src }
+              className='lightbox-img-thumbnail'
+              style={{width: geometory.boxes[idx].width, height: geometory.boxes[idx].height, top: geometory.boxes[idx].top, left: geometory.boxes[idx].left, position: "absolute"}}
+              alt={image.title}
+              onClick={toggleLightbox.bind(null, idx)} />
+          );
+        };
+        return (
+          <div style={{position: "absolute"}}>
+            <Lightbox.Lightbox images={imgs} renderImageFunc={renderImageFunc}/>
+          </div>
         );
     }
 };
