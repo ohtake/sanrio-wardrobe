8a37b25ec4075782369c42d4c5b47db7e4b13740
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2016-05-29 08:56:23 +0900

    Add color selector

--- app.js
+++ app.js
@@ -26,6 +26,96 @@ class Photo {
     }
 }
 
+class ColorItem {
+    constructor(id, name, strong, weak) {
+        this.id = id
+        this.name = name;
+        this.strong = strong;
+        this.weak = weak;
+        this.active = false;
+    }
+    toggle() {
+        this.active = !this.active;
+    }
+}
+
+class ColorSelector extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {colors: props.colors};
+        this.onChanged = props.onChanged;
+    }
+    styleBase() {
+        return {
+            display: "inline-block",
+            margin: "0.2em 0",
+            padding: "0.2em 0.5em",
+            textDecoration: "none",
+            color: "black",
+            borderWidth: "0 0 0.25em",
+        }
+    }
+    styleColor(c) {
+        let style = this.styleBase();
+        style.borderStyle = "solid";
+        style.borderColor = c.active ? c.strong : c.weak;
+        style.backgroundColor = c.active ? c.weak : "transparent";
+        return style;
+    }
+    toggle(c, e){
+        e.preventDefault();
+        c.toggle();
+        this.onChanged(this);
+        this.setState({colors: this.state.colors})
+    }
+    clear(e) {
+        if (e) e.preventDefault();
+        for (let c of this.state.colors) {
+            c.active = false;
+        }
+        this.onChanged(this);
+        this.setState({colors: this.state.colors});
+    }
+    listActiveIds() {
+        return this.state.colors.filter(c => {return c.active; }).map(c => {return c.id; });
+    }
+    render() {
+        return <div>
+          <span style={this.styleBase()}>Color filter</span>
+          { this.state.colors.map((c,i) => {
+              return <a href="#" style={this.styleColor(c)} onClick={this.toggle.bind(this,c)}>
+                  {c.name}
+              </a>
+          }) }
+          <a href="#" onClick={this.clear.bind(this)} style={this.styleBase()}>CLEAR</a>
+        </div>;
+    }
+}
+ColorSelector.propTypes = {
+    colors: React.PropTypes.arrayOf(React.PropTypes.instanceOf(ColorItem)),
+    onChanged: React.PropTypes.func,
+}
+ColorSelector.defaultProps = {
+    colors: [
+        // Color values are taken from http://www.google.com/design/spec/style/color.html#color-color-palette
+        // strong=500, weak=200
+        new ColorItem("red", "Red", "#f44336", "#ef9a9a"),
+        new ColorItem("pink", "Pink", "#e91e63", "#f48fb1"),
+        new ColorItem("orange", "Orange", "#ff9800", "#ffcc80"),
+        new ColorItem("yellow", "Yellow", "#ffeb3b", "#fff59d"),
+        new ColorItem("green", "Green", "#4caf50", "#a5d6a7"),
+        new ColorItem("blue", "Blue", "#2196f3", "#90caf9"),
+        new ColorItem("purple", "Purple", "#9c27b0", "#ce93d8"),
+        new ColorItem("brown", "Brown", "#795548", "#bcaaa4"),
+        new ColorItem("black", "Black", "#9e9e9e", "#eeeeee"),
+        new ColorItem("grey", "Grey", "#9e9e9e", "#eeeeee"),
+        new ColorItem("silver", "Silver", "#9e9e9e", "#eeeeee"),
+        new ColorItem("white", "White", "#9e9e9e", "#eeeeee"),
+        new ColorItem("gold", "Gold", "#ffc107", "#ffe082"),
+    ],
+    onChanged: function(sender){},
+}
+
 class App extends React.Component{
     constructor(){
         super();
@@ -73,9 +163,24 @@ class App extends React.Component{
             });
         });
     }
+    colorChanged(sender) {
+        let colors = sender.listActiveIds();
+        if (colors.length == 0 ){
+            this.setState({photos: this.state.allPhotos});
+        } else {
+            let photos = this.state.allPhotos.filter(p => {
+                for (let c of colors) {
+                    if (p.data.tags.indexOf("color:"+c) < 0) return false;
+                }
+                return true;
+            });
+            this.setState({photos: photos});
+        }
+    }
     render(){
         return(
             <div className="App">
+                <ColorSelector onChanged={this.colorChanged.bind(this)} />
                 {this.state.message ? <div>{this.state.message}</div> : null}
                 {this.state.photos ? this.renderGallery() : null}
                 {this.state.isOpen ? this.renderLightbox() : null }
