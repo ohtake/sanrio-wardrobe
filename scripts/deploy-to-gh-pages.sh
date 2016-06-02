4abdc97860e1254e4ac8f3ed790ae080cfbed78f
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2016-06-02 16:54:19 +0900

    Let Travis deploy release branch to gh-pages branch

---
+++ scripts/deploy-to-gh-pages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+git checkout -B gh-pages
+git add assets -f
+if [ "true" = "${TRAVIS}" ]; then
+  git config user.name "travis"
+  git config user.email "travis@example.net"
+fi
+git commit -m build
+# Needs --quiet to hide authentication token 
+git push "${GIT_REMOTE_URL_WITH_AUTH}" gh-pages -f --quiet
+git checkout -
