f88eb5504ac40d344923ed949df276dec12b927f
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2016-06-11 23:27:06 +0900

    Add GitHub ribbon aka Fork me on GitHub

--- src/home.jsx
+++ src/home.jsx
@@ -149,7 +149,15 @@ export default class Home extends React.Component {
   render() {
     return (
       <div>
-        <p>You can find clothings of Sanrio characters.</p>
+        <a href="https://github.com/ohtake/sanrio-wardrobe">
+          <img
+            alt="Fork me on GitHub"
+            style={{ position: 'absolute', top: this.context.muiTheme.appBar.height, right: 0, border: 0 }}
+            src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67"
+            data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+          />
+        </a>
+        <p style={{ paddingRight: '150px' }}>You can find clothings of Sanrio characters.</p>
         <h2>Characters</h2>
         <div ref="grid">
           <JustifiedLayout targetRowHeight={150} containerPadding={0} boxSpacing={6} containerWidth={this.state.containerWidth}>
@@ -170,3 +178,6 @@ export default class Home extends React.Component {
     );
   }
 }
+Home.contextTypes = {
+  muiTheme: React.PropTypes.object.isRequired,
+};
