7be60a3066a4d87c8440065f2e4133313f8d51a9
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2016-06-22 17:31:16 +0900

    Easier way to use ES6 in Mocha
    You can specify test files, e.g. npm run test -- test/foo.spec.js
    You can watch test, e.g. npm run test:watch

--- test/index.spec.js
+++
@@ -1,9 +0,0 @@
-import Mocha from 'mocha';
-
-const mocha = new Mocha();
-mocha.addFile('test/data_file.spec.js');
-mocha.run((failures) => {
-  process.on('exit', () => {
-    process.exit(failures);
-  });
-});
