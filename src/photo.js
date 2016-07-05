27c09eb7cc9203889b563adec6fba7a79c5b2d12
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2016-07-05 22:34:38 +0900

    Support srcset generation for Flickr

--- src/photo.js
+++ src/photo.js
@@ -1,3 +1,73 @@
+const flickrSizesAll = [
+  { suffix: 's', longest: 75, square: true, label: 'Square' },
+  { suffix: 'q', longest: 150, square: true, label: 'Large Square' },
+  { suffix: 't', longest: 100, label: 'Thumbnail' },
+  { suffix: 'm', longest: 240, label: 'Small' },
+  { suffix: 'n', longest: 320, label: 'Small 320' },
+  { suffix: '', longest: 500, label: 'Medium' },
+  { suffix: 'z', longest: 640, label: 'Medium 640' },
+  { suffix: 'c', longest: 800, since: '20120301', label: 'Medium 800' },
+  { suffix: 'b', longest: 1024, since: '20100525', label: 'Large' },
+  { suffix: 'h', longest: 1600, since: '20120301', label: 'Large 1600' },
+  { suffix: 'k', longest: 2048, since: '20120301', label: 'Large 2048' },
+  { suffix: 'o', customExtention: true, label: 'Original' },
+];
+const flickrSizes = flickrSizesAll.filter(s => ! s.square).filter(s => s.longest);
+
+/*
+Example:
+images_flickr: { photo: "15605014039", farm: 6, server: 5604, secret: "3aa65f125e", secret_h: "2c5f7cc664", secret_k: "31513b8bf6" }
+
+Parameters:
+  photo: string required
+  farm: number/string required
+  server: number/string required
+  secret: string required
+  secret_h: string optional
+  secret_k: string optional
+  before: enum('20100525' | '20120301') optional
+    If '20100525', c, b, h, and k are not available.
+    If '20120301', c, h, and k are not available.
+Parmeters not supported:
+  secret_o: string optional
+  format_o: enum('jpg' | 'gif' | 'png') optional
+*/
+class FlickrSrcsetProvider {
+  static createUrl(photoId, farmId, serverId, secret, suffix) {
+    let suffix2 = '';
+    if (suffix) suffix2 = `_${suffix}`;
+    return `https://farm${farmId}.staticflickr.com/${serverId}/${photoId}_${secret}${suffix2}.jpg`;
+  }
+  static getImages(photo) {
+    const maxO = Math.max(photo.data.size.width_o, photo.data.size.height_o);
+    const imgsF = photo.data.images_flickr;
+    const result = [];
+    for (const size of flickrSizes) {
+      let secret;
+      switch (size.suffix) {
+        case 'h':
+          if (! imgsF.secret_h) continue;
+          secret = imgsF.secret_h;
+          break;
+        case 'k':
+          if (! imgsF.secret_k) continue;
+          secret = imgsF.secret_k;
+          break;
+        case 'o':
+          throw new Error('Not supported');
+        default:
+          if (imgsF.before && size.since && imgsF.before <= size.since) continue;
+          secret = imgsF.secret;
+          break;
+      }
+      const url = FlickrSrcsetProvider.createUrl(imgsF.photo, imgsF.farm, imgsF.server, secret, size.suffix);
+      const max = Math.min(maxO, size.longest);
+      result.push({ url, max });
+    }
+    return result;
+  }
+}
+
 export default class Photo {
   constructor(data) {
     this.data = data;
@@ -29,6 +99,8 @@ export default class Photo {
     let images;
     if (this.data.images) {
       images = this.data.images.map(i => this.prepareSize(i));
+    } else if (this.data.images_flickr) {
+      images = FlickrSrcsetProvider.getImages(this).map(i => this.prepareSize(i));
     } else {
       images = [this.prepareSize({ url: this.data.image, max: this.data.size.max_len })];
       const largeUrl = this.inferLargeImage();
--- test/photo.spec.js
+++ test/photo.spec.js
@@ -55,6 +55,30 @@ const photoSrcsetSquare = {
   colors: [],
   notes: [],
 };
+const photoFlickrLandscape = {
+  title: 'flickr images landscape',
+  images_flickr: { photo: '15605014039', farm: 6, server: 5604, secret: '3aa65f125e', secret_h: '2c5f7cc664', secret_k: '31513b8bf6' },
+  source: { author: 'ohtake', license: 'CC BY-SA 2.0', url: 'https://www.flickr.com/photos/ohtake_tomohiro/15605014039' },
+  size: { width_o: 5760, height_o: 3840 },
+  colors: [],
+  notes: [],
+};
+const photoFlickrPortrait = {
+  title: 'flickr images portrait',
+  images_flickr: { photo: '24326364382', farm: 2, server: 1623, secret: '2a00bf3387', secret_h: '988fe74e0e', secret_k: '1d900d84de' },
+  source: { author: 'ohtake', license: 'CC BY-SA 2.0', url: 'https://www.flickr.com/photos/ohtake_tomohiro/24326364382' },
+  size: { width_o: 3840, height_o: 5760 },
+  colors: [],
+  notes: [],
+};
+const photoFlickrTooSmall = {
+  title: '24214891894',
+  images_flickr: { photo: '24214891894', farm: 2, server: 1457, secret: 'fb5e07fc69', before: '20100525' },
+  source: { author: 'jj lai', license: '', url: 'https://www.flickr.com/photos/laijj/24214891894/' },
+  size: { width_o: 538, height_o: 404 },
+  colors: [],
+  notes: [],
+};
 
 /* eslint-disable no-unused-expressions */
 /* eslint-disable prefer-arrow-callback, func-names */
@@ -113,6 +137,44 @@ describe('Photo', function () {
     it('should return for srcset', function () {
       const photo = new Photo(photoSrcsetLandscape);
       expect(photo.getSrcSet()).to.equal('https://placehold.it/600x400 600w, https://placehold.it/900x600 900w, https://placehold.it/1200x800 1200w');
+    });
+    it('should return for flickr landscape images', function () {
+      const photo = new Photo(photoFlickrLandscape);
+      expect(photo.getSrcSet()).to.equal([
+        'https://farm6.staticflickr.com/5604/15605014039_3aa65f125e_t.jpg 100w',
+        'https://farm6.staticflickr.com/5604/15605014039_3aa65f125e_m.jpg 240w',
+        'https://farm6.staticflickr.com/5604/15605014039_3aa65f125e_n.jpg 320w',
+        'https://farm6.staticflickr.com/5604/15605014039_3aa65f125e.jpg 500w',
+        'https://farm6.staticflickr.com/5604/15605014039_3aa65f125e_z.jpg 640w',
+        'https://farm6.staticflickr.com/5604/15605014039_3aa65f125e_c.jpg 800w',
+        'https://farm6.staticflickr.com/5604/15605014039_3aa65f125e_b.jpg 1024w',
+        'https://farm6.staticflickr.com/5604/15605014039_2c5f7cc664_h.jpg 1600w',
+        'https://farm6.staticflickr.com/5604/15605014039_31513b8bf6_k.jpg 2048w',
+      ].join(', '));
+    });
+    it('should return for flickr portrait images', function () {
+      const photo = new Photo(photoFlickrPortrait);
+      expect(photo.getSrcSet()).to.equal([
+        'https://farm2.staticflickr.com/1623/24326364382_2a00bf3387_t.jpg 67w',
+        'https://farm2.staticflickr.com/1623/24326364382_2a00bf3387_m.jpg 160w',
+        'https://farm2.staticflickr.com/1623/24326364382_2a00bf3387_n.jpg 213w',
+        'https://farm2.staticflickr.com/1623/24326364382_2a00bf3387.jpg 333w',
+        'https://farm2.staticflickr.com/1623/24326364382_2a00bf3387_z.jpg 427w',
+        'https://farm2.staticflickr.com/1623/24326364382_2a00bf3387_c.jpg 533w', // 534 is corrent in Flickr. See <https://twitter.com/ohtaket/status/750318571926859776>
+        'https://farm2.staticflickr.com/1623/24326364382_2a00bf3387_b.jpg 683w',
+        'https://farm2.staticflickr.com/1623/24326364382_988fe74e0e_h.jpg 1067w',
+        'https://farm2.staticflickr.com/1623/24326364382_1d900d84de_k.jpg 1365w',
+      ].join(', '));
+    });
+    it('should return for flickr too small', function () {
+      const photo = new Photo(photoFlickrTooSmall);
+      expect(photo.getSrcSet()).to.equal([
+        'https://farm2.staticflickr.com/1457/24214891894_fb5e07fc69_t.jpg 100w',
+        'https://farm2.staticflickr.com/1457/24214891894_fb5e07fc69_m.jpg 240w',
+        'https://farm2.staticflickr.com/1457/24214891894_fb5e07fc69_n.jpg 320w',
+        'https://farm2.staticflickr.com/1457/24214891894_fb5e07fc69.jpg 500w',
+        'https://farm2.staticflickr.com/1457/24214891894_fb5e07fc69_z.jpg 538w',
+      ].join(', '));
     });
   });
   describe('#prepareSize', function () {
