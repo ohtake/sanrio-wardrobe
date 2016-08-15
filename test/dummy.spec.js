53644d92aa483ac6ec6ec4f9e9004dc49364b5d5
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2016-08-15 22:07:38 +0900

    Good-bye mocha and chai

--- test/dummy.spec.js
+++
@@ -1,8 +0,0 @@
-import { describe, it } from 'mocha';
-import { expect } from 'chai';
-
-describe('Dummy', function () {
-  it('should be true', function () {
-    expect(true).to.be.true;
-  });
-});
