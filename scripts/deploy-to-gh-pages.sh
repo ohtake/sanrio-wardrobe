c844ee178a318094748f5b7a943e006a17af3532
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2017-02-21 15:00:27 +0900

    Use deploy lifecycle in Travis

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -4,6 +4,8 @@ set -o nounset
 set -o pipefail
 
 git checkout -B gh-pages
+npm run build
+# npm run cover # already executed in run-travis.sh
 git add assets coverage/lcov-report -f
 if [ "true" = "${TRAVIS}" ]; then
   git config user.name "travis"
