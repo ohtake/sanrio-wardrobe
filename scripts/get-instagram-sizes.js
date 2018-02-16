8c1df5d70ab8a26f9fc6306d14ba52a5bca18f0c
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2018-02-17 00:44:42 +0900

    Remove Instagram v1 srcset provider

--- scripts/get-instagram-sizes.js
+++ scripts/get-instagram-sizes.js
@@ -44,14 +44,11 @@ async function main() {
   const endpoints = sizes.map(s => `https://www.instagram.com/p/${shortcode}/media/?size=${s}`);
   const urls = await Promise.all(endpoints.map(e => getRedirectLocation(e)));
   const jpegs = await Promise.all(urls.map(u => fetchAndDecode(u)));
-  const params = [];
   const params2 = [];
-  params.push(`shortcode: "${shortcode}"`);
   params2.push(`shortcode: "${shortcode}"`);
   let widthL;
   let heightL;
   jpegs.forEach((jp, i) => {
-    params.push(`width_${sizes[i]}: ${jp.width}`);
     if (i === 2) {
       widthL = jp.width;
       heightL = jp.height;
@@ -61,7 +58,6 @@ async function main() {
     }
     params2.push(`${sizes[i]}_url: "${urls[i]}"`);
   });
-  console.log(`images_instagram: { ${params.join(', ')} }`);
   console.log(`images_instagram2: { ${params2.join(', ')} }`);
   console.log(`size: { width_o: ${widthL}, height_o: ${heightL} }`);
 }
